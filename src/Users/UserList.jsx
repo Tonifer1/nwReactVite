d2ede0b2869dec0ba0e6510826a4ae7f94fe48cf
Toni Pekkala <toni.pekkala@hotmail.com>
2024-10-28 23:33:02 +0200

    Lisätty User toiminnot ja lisätty taulukko.

--- src/Users/UserList.jsx
+++ src/Users/UserList.jsx
@@ -12,7 +12,7 @@ import UserService from '../Services/UserServ'
 // import User from './user'
 
 // Tuodaan CustomerAdd-komponentti (Funktion nimi), joka mahdollistaa uuden asiakkaan lisäämisen
-// import UserAdd from './userAdd';
+ import UserAdd from './userAdd';
 
 // import UserEdit from './userEdit'
 
@@ -25,16 +25,12 @@ const UserList = ({setIsPositive, setMessage, setShowMessage,}) => {
     // Määritellään tila eli state users, joka sisältää userslistan.Alempana tarkistetaan onko dataa renderöitäväksi.
     const [users, setUsers] = useState([])
 
-    // Määritellään tila eli state lisäystila, joka määrittää näytetäänkö CustomerAdd vai ei.
+    // Määritellään tila eli state lisäystila, joka määrittää näytetäänkö UserAdd vai ei.
     const [lisäystila, setLisäystila] = useState(false)
-
     const [muokkaustila, setMuokkaustila] = useState(false)
-
     const [muokattavaUser, setMuokattavaUser] = useState(false)
-
     const [search, setSearch] = useState('')
-
-    const [reload, reloadNow] = useState(false) 
+    //const [reload, reloadNow] = useState(false) 
 
     //useEffect-hook, joka hakee heti asiakastiedot ohjelman käynnistyessä CustomerService:ltä komponentin latautuessa. Tässä tulee Axios käyttöön.
     //Axios osaa konvertoida JSON datan suoraan JavaScriptiksi.
@@ -43,17 +39,16 @@ const UserList = ({setIsPositive, setMessage, setShowMessage,}) => {
     useEffect(() => {
         UserService.getAll()
             .then(data =>  setUsers(data)) // Asetetaan haetut asiakastiedot users-tilaan.(data) tulee UserServ.js tiedostosta.
-    }, [lisäystila, muokkaustila])//reload olisi tässä 
-
+    }, [lisäystila, muokkaustila])
     //Hakukentän onChange tapahtumankäsittelijä. Parametrina event, joka edustaa input-kentän tapahtumaa. 
     const handleSearchInputChange = (event) => {        
         setSearch(event.target.value.toLowerCase())
     }
     
-    // const editUser = (userprops) => {
-    //     setMuokattavaUser(userprops)
-    //     setMuokkaustila(true)
-    // }
+    const editUser = (userprops) => {
+        setMuokattavaUser(userprops)
+        setMuokkaustila(true)
+    }
 
 return (
     <>                
@@ -64,6 +59,16 @@ return (
             }
         </div>
 
+        {lisäystila && ( <UserAdd setLisäystila= {setLisäystila} setUsers={setUsers}
+         setIsPositive={setIsPositive} setMessage={setMessage} setShowMessage={setShowMessage}/>                  
+        )}
+
+        <span className="nowrap">
+            <button className="nappi" style={{ cursor: 'pointer' }} onClick={() => setLisäystila(!lisäystila)}>      
+                {lisäystila ? "Hide Add User" : "Show Add User"}
+            </button>
+        </span>
+
         {/* <span className="nowrap">
         {(!lisäystila && !muokkaustila) && (
             <button className="nappi" style={{ cursor: 'pointer' }} onClick={() => setShow(!show)}>                        
@@ -71,56 +76,49 @@ return (
         
         </span> */}
 
-        {/* <span className="nowrap">
-            <button className="nappi" style={{ cursor: 'pointer' }} onClick={() => setLisäystila(!lisäystila)}>      
-                {lisäystila ? "Hide Add Customer" : "Show Add Customer"}
-            </button>
-        </span> */}
-
-        {/* PROPSIT  kolmeen tiedostoon. --> customerAdd,customerEdit, customer */}
-        {/* Renderöidään CustomerAdd-komponentti, kun lisäystila on true. Mahdollistaa uuden asiakaan lisäyksen */}
-
-        {/* {lisäystila && ( <CustomerAdd setLisäystila= {setLisäystila} setCustomers={setCustomers}
-        setIsPositive={setIsPositive} setMessage={setMessage} setShowMessage={setShowMessage}/>                  
-        )}
-
+        {/*
         {muokkaustila && ( <CustomerEdit setMuokkaustila ={setMuokkaustila} 
         setIsPositive={setIsPositive} setMessage={setMessage} setShowMessage={setShowMessage}
         muokattavaCustomer={muokattavaCustomer} setCustomers={setCustomers} />                    
         )} */}
+        {!lisäystila && !muokkaustila && (
+    <div className="text-center table-responsive">
+        <table className="table table-striped table-bordered table-hover mx-auto">
+            <thead className="table-dark">
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Username</th>
+                    <th>Email</th>                                
+                    <th>Acceslevel</th>
+                </tr>
+            </thead>
+            <tbody>                                           
+                {users && users.map(u => {
+                    const lowerCaseName = u.lastName ? u.lastName.toLowerCase() : '';
+                    if (lowerCaseName.indexOf(search) > -1) {
+                        return (
+                            <tr key={u.userId || u.email || u.username}>
+                                <td>{u.firstName}</td>
+                                <td>{u.lastName}</td>
+                                <td>{u.username}</td>
+                                <td>{u.email}</td>
+                                <td>{u.acceslevelId}</td>
+                            </tr>
+                        ); 
+                    } 
+                    return null;
+                })}
+            </tbody>
+        </table>
+    </div>
+)}
+
     
-    <table>
-    <thead>
-        <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-            <th>Email</th>                                
-            <th>Acceslevel</th>
-        </tr>
-    </thead>
-    <tbody>                                           
-        {users && users.map(u => {
-            const lowerCaseName = u.lastName.toLowerCase();
-            if (lowerCaseName.indexOf(search) > -1) {
-                return (
-                    <tr key={u.userId}>
-                        <td>{u.firstName}</td>
-                        <td>{u.lastName}</td>
-                        <td>{u.username}</td>
-                        <td>{u.email}</td>
-                        <td>{u.acceslevelId}</td>
-                    </tr>
-                ); 
-            } 
-            return null;
-        })}
-    </tbody>
-</table>   
-    </>
-)//return
+               
+        </>
+    )//return
     
 }//UserList
 
-// Exportataan UserList-komponentti, jotta sitä voidaan käyttää muualla sovelluksessa
 export default UserList
---
+++ src/Users/userAdd.jsx
@@ -0,0 +1,114 @@
+import '../App.css'
+import React, { useState } from 'react'
+import UserService from '../Services/UserServ'
+import md5 from 'md5'
+
+const UserAdd = ({ setLisäystila, setIsPositive, setUsers, setMessage, setShowMessage, }) => {
+
+    //! ********************Tilan eli Staten määritys*************************************
+    // Statet pitävät kirjaa sen hetken tilasta ja päivittävät sitä, joka kerta kun käyttäjä kirjoittaa jotain kenttään.
+    // Esim Id kenttään kirjoitettaessa, kutsutaan setNewCustomerId funktiota(alla returnissa), joka päivittää tilan newCustomerId arvon.
+
+    
+    const [newFirstName, setNewFirstName] = useState('')
+    const [newLastName, setNewLastname] = useState('')
+    const [newEmail, setNewEmail] = useState('')
+    const [newAcceslevelId, setNewAcceslevelId] = useState(0)
+
+    const [newUsername, setNewUsername] = useState('')
+    const [newPassword, setNewPassword] = useState('')
+    
+    
+    // 2.  Syötetyt tiedot kerätään ja luodaan uusi asiakasobjekti(newCustomer), johon tiedot tallennetaan.
+    //! ********************onSubmit tapahtumankäsittelijä funktio*****************************************************
+    // event.preventDefault() estää lomakkeen lähettämisen yhteydessä kokokonaisen sivun uudelleen lataamisen.
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        // Alla olevat kentät täytyy olla nimeltään samat kuin back-endissä olevat kentät. Huom! camelCase.
+        var newUser = {
+            firstname: newFirstName,
+            lastname: newLastName,
+            email: newEmail,
+            acceslevelId:newAcceslevelId,
+            username: newUsername,
+            password: md5(newPassword)
+        }//newUser
+
+        
+        console.log("newAcceslevelId ennen Userserviceä:", newAcceslevelId);
+
+        UserService.addNew(newUser)
+            .then(() => {
+                console.log("Sending new user to backend:", newUser);
+                setMessage(`Lisätty new user:${newUser.firstname} ${newUser.lastname}`)
+                setIsPositive(true);
+                setShowMessage(true);
+                setUsers(prevUsers => [...prevUsers, newUser])
+                console.log("Lisattu uusi luuseri Servicen sisällä:",)
+
+                setTimeout(() => {
+                    setShowMessage(false);
+                }, 3000);
+
+                setLisäystila(false);
+                
+            })//then
+
+            .catch(error => {
+                console.error("Error to Add New User:", error);
+            });
+    
+    }//handleSubmit
+
+    //! ****************************return*************************************
+    return (
+        <div id="addNew">
+            <h2>User add</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <input type="text" value={newFirstName} placeholder="First name"
+                        onChange={({ target }) => setNewFirstName (target.value)} required />
+                </div>
+                <div>
+                    <input type="text" value={newLastName} placeholder="Last name"
+                        onChange={({ target }) => setNewLastname (target.value)} required />
+                </div>
+                <div>
+                    <input type="email" value={newEmail} placeholder="Email"
+                        onChange={({ target }) => setNewEmail(target.value)} />
+                </div>
+                <div>
+                    <input type="number" value={newAcceslevelId} placeholder="AcceslevelId"
+                        onChange={({ target }) => setNewAcceslevelId(Number(target.value))} />
+                </div>
+                <div>
+                    <input type="text" value={newUsername} placeholder="Username"
+                        onChange={({ target }) => setNewUsername(target.value)} />
+                </div>
+                <div>
+                    <input type="password" value={newPassword} placeholder="Password"
+                        onChange={({ target }) => setNewPassword(target.value)} />
+                </div>
+               
+
+                <div style={{ marginTop: '20px' }}>
+                    {/* 1. */}
+                    {/* Tämä on submit-tyyppinen input-elementti, joka lähettää lomakkeen, kun sitä(save) klikataan */}
+
+                    {/* Ohjelman suoritus siirtyy tästä ylhäällä olevaan handleSubmit-funktioon */}
+
+                    <input type='submit' value='save' style={{ marginRight: '10px' }} />
+
+                    {/* Tämä on tavallinen button-tyyppinen input-elementti, joka ei lähetä lomaketta */}
+                    <input type='button' value='back' onClick={() => setLisäystila(false)} />
+                </div>
+            </form>
+
+        </div>
+
+    )//return
+
+}//UserAdd
+
+export default UserAdd
